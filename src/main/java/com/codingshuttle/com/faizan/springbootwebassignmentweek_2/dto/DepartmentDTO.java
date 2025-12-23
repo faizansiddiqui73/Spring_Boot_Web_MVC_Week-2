6e73c2554c04fa0a77580eca6608308327ba5826
Faizan Siddiqui <faizansiddiqui73@gmail.com>
2025-12-23 13:16:33 +0530

    Revert "Updated comments in Global exception handler"
    This reverts commit 108ce6418979e91ec7a039a3c0d0e3e01796974b.

--- src/main/java/com/codingshuttle/com/faizan/springbootwebassignmentweek_2/dto/DepartmentDTO.java
+++ src/main/java/com/codingshuttle/com/faizan/springbootwebassignmentweek_2/dto/DepartmentDTO.java
@@ -75,7 +75,9 @@ public class DepartmentDTO {
     //For API Checking
     /*
  {
-  DepartementRepository
+  "title" : "Electronics",
+  "email" : "electronics@gmail.com",
+  "isActive":true,
   "createdAt":"2024-07-23",
   "salary" : 54000.00,
   "hiredAt":"2021-09-09",
